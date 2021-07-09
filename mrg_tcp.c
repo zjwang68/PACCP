9f8115124bd973911f6bb7f3100b89fcf52ff5a3
zjwang68 <64947589+zjwang68@users.noreply.github.com>
2021-07-09 10:48:43 -0500

    Update mrg_tcp.c

--- mrg_tcp.c
+++ mrg_tcp.c
@@ -1,43 +1,5 @@
 /* Minimum Rate Guaranteed MRG) congestion control.
- *
- * http://simula.stanford.edu/~alizade/Site/DCTCP.html
- *
- * This is an implementation of DCTCP over Reno, an enhancement to the
- * TCP congestion control algorithm designed for data centers. DCTCP
- * leverages Explicit Congestion Notification (ECN) in the network to
- * provide multi-bit feedback to the end hosts. DCTCP's goal is to meet
- * the following three data center transport requirements:
- *
- *  - High burst tolerance (incast due to partition/aggregate)
- *  - Low latency (short flows, queries)
- *  - High throughput (continuous data updates, large file transfers)
- *    with commodity shallow buffered switches
- *
- * The algorithm is described in detail in the following two papers:
- *
- * 1) Mohammad Alizadeh, Albert Greenberg, David A. Maltz, Jitendra Padhye,
- *    Parveen Patel, Balaji Prabhakar, Sudipta Sengupta, and Murari Sridharan:
- *      "Data Center TCP (DCTCP)", Data Center Networks session
- *      Proc. ACM SIGCOMM, New Delhi, 2010.
- *   http://simula.stanford.edu/~alizade/Site/DCTCP_files/dctcp-final.pdf
- *
- * 2) Mohammad Alizadeh, Adel Javanmard, and Balaji Prabhakar:
- *      "Analysis of DCTCP: Stability, Convergence, and Fairness"
- *      Proc. ACM SIGMETRICS, San Jose, 2011.
- *   http://simula.stanford.edu/~alizade/Site/DCTCP_files/dctcp_analysis-full.pdf
- *
- * Initial prototype from Abdul Kabbani, Masato Yasuda and Mohammad Alizadeh.
- *
- * Authors:
- *
- *	Daniel Borkmann <dborkman@redhat.com>
- *	Florian Westphal <fw@strlen.de>
- *	Glenn Judd <glenn.judd@morganstanley.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+Modified Linux implementation based on D2TCP code by Yunxiang Wu
  */
 
 #include <linux/module.h>
